4fbe4e5f1fddc7504a07e9d735d826e7eb6f2231
okonomi <okonomi@oknm.jp>
2022-09-27 14:34:39 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://okonomi.github.io',
-  // base: '/astro-handson',
+  base: '/astro-handson',
 	integrations: [mdx(), react(), sitemap()],
   trailingSlash: 'never'
 });
